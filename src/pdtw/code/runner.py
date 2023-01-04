75d8beb23bd542460d8e0647c609ee7c6fdf251b
Stefano Vittorio Porta <stefa168@hotmail.it>
2023-01-04 11:00:42 +0100

    feat: results

--- src/pdtw/code/runner.py
+++ src/pdtw/code/runner.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import re
 from typing import Pattern, List, TextIO
@@ -21,16 +22,26 @@ ACTUAL_SCENARIO_ID	%(scenario_id)d
 TW_TYPE			d1
 """
 
+bb_timedelta = datetime.timedelta(0)
+br_timedelta = datetime.timedelta(0)
+
 for s in problem_instances:
     for n in range(1, number_of_scenarios_to_run + 1):
         config_file: TextIO = open("../instances/sddp/stacy/config/temp.dat", "w")
         config_file.write(template % {"problem": s, "scenario_id": n})
         config_file.close()
 
+        start = datetime.datetime.now()
         os.system(
-            "./cmake-build-debug/branch_and_bound_solver ../instances/sddp/stacy/config/temp.dat"
-            )
+            "./cmake-build-debug-wsl/branch_and_bound_solver ../instances/sddp/stacy/config/temp.dat"
+        )
+        bb_timedelta += datetime.datetime.now() - start
 
-        #os.system(
-        #    "./cmake-build-debug-wsl/branch_and_regret_solver ../instances/sddp/stacy/config/temp.dat"
-        #)
+        start = datetime.datetime.now()
+
+        os.system(
+            "./cmake-build-debug-wsl/branch_and_regret_solver ../instances/sddp/stacy/config/temp.dat"
+        )
+        br_timedelta += datetime.datetime.now() - start
+
+print(f"Computation took {str(bb_timedelta+br_timedelta)}, BB: {str(bb_timedelta)}, BR: {str(br_timedelta)}")
